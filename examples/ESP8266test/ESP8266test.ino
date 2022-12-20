cd2b64dab890bb3af892af6ce86be33573fb3c80
piedor <dorighi.pietro@hotmail.it>
2022-12-20 21:31:48 +0100

    Added examples folder

---
+++ examples/ESP8266test/ESP8266test.ino
@@ -0,0 +1,31 @@
+#include <ESP8266_01.h>
+
+#define SSID "ssid"
+#define PASSWORD "password"
+
+ESP8266_01 esp;
+
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(115200);
+
+  // ESP8266 init (serial esp, serial debug, debug on) 
+  esp = ESP8266_01(Serial1, Serial, true);
+  // Set as client only
+  esp.setAsStation();
+  // Connect to wifi loop
+  while(esp.isConnected().equals("")){
+    esp.connectWifi(SSID, PASSWORD);
+  }
+  // Enable HTTP 302 redirect link
+  esp.enableAutoRedirectUrl();
+  // Get website request response
+  // getSecureConnection( url, request address, port, wait response on, keep-alive connection on (for fast interaction))
+  String response = esp.getSecureConnection("www.google.com", "/", 443, true, true);
+  Serial.println(response);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
